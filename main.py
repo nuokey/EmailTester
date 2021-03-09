dfeb275e4ba3c6d0b7e8f53ef9ab629029c387f7
Timo <66961498+nuokey@users.noreply.github.com>
2021-03-09 20:10:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
-email = input()
+def check(email):
+    try:
+        assert '@' in email
 
-if '@' not in email:
-    print(False)
-    exit()
+        assert '.' in email
 
-if '.' not in email:
-    print(False)
-    exit()
+        yes_in = 'qwertyuiopasdfghjklzxcvbnm' + '@-_.'
+        for i in range(len(email)):
+            assert email[i] in yes_in
 
-yes_in = 'qwertyuiopasdfghjklzxcvbnm' + '@-_.'
-for i in range(len(email)):
-    if email[i] not in email:
-        print(False)
+        email_1 = email.split('@')
+        assert '.' in email_1[1]
+
+        email_2 = email.split('.')
+        assert len(email_2[len(email_2)-1]) >= 2
+        assert len(email_2[len(email_2)-1]) <= 4
+
+        email_3 = email_1[1].split('.')
+        assert len(email_3[0]) >= 2
+
+        assert len(email_1[0]) >= 4
+
+    except AssertionError:
+        return(False)
         exit()
 
-email_1 = email.split('@')
-if '.' not in email_1[1]:
-    print(False)
-    exit()
+    except TypeError:
+        return(False)
+        exit()
 
-email_2 = email.split('.')
-if len(email_2[len(email_2)-1]) < 2 or len(email_2[len(email_2)-1]) > 4:
-    print(False)
-    exit()
-
-email_3 = email_1[1].split('.')
-if len(email_3[0]) < 2:
-    print(False)
-    exit()
-
-if len(email_1[0]) < 4:
-    print(False)
-    exit()
-
-print(True)
+    return(True)
